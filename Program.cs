259250dee2c9eccf2829f42e4ef7a3f40b4405e1
yanepridumala3000 <askrapivkina@gmail.com>
2026-05-03 14:37:40 +0000

    Add CI workflow

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -84,7 +85,8 @@ app.MapGet("/version", (IConfiguration conf) => new {
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
